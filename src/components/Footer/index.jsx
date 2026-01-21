051cc14aa107876ecfa9905e33fe81ce84df003e
fazliddin10-up <majidovfazliddin85@gmail.com>
2026-01-21 13:33:21 +0300

    acw

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,9 +2,54 @@
 import './style.css'
 
 function Footer() {
-    return ( 
-        <h3>Footer</h3>
-     );
+    return (
+        <footer className='fazliddinbg' >
+            <div>
+                <h4>КАТАЛОГ ТОВАРОВ</h4>
+                <ul>
+                    <li>Кардио тренажеры</li>
+                    <li>Composite Strength</li>
+                    <li>True Stretch</li>
+                    <li>Сайклинг</li>
+                    <li>Групповые тренировки</li>
+                    <li>Силовые тренажеры</li>
+                    <li>Консоли</li>
+                    <li><b> © TRUE FITNESS</b></li>
+                </ul>
+            </div>
+
+
+            <div>
+                <h4>ИНФОРМАЦИЯ</h4>
+                <ul>
+                    <li>Бренде</li>
+                    <li>Преимущества</li>
+                    <li>открыть клуб</li>
+                    <li>продукции</li>
+                    <li>контакты</li>
+                </ul>
+            </div>
+
+
+            <div>
+
+                <h4>КОНТАКТЫ</h4>
+                <ul>
+                    <li>Политика конфиденциальности</li>
+                    <li>Контакты</li>
+                </ul>
+            </div>
+
+
+            <div>
+                <h4 className='kamoliddin' >ПОДПИСАТЬСЯ НА НОВОСТИ И АКЦИИ</h4>
+            </div>
+
+
+        </footer>
+
+
+    );
 }
 
 export default Footer;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,20 +2,40 @@
 import './style.css'
 import logo from "../../assets/logo.png"
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Header() {
+
     return (
-        <header>
+        <header className='contayner' >
 
-            <div>
-                <Link to="/" >
-                    <img src={logo} alt="" />
-                </Link>
+            <div className='fazliddin' >
+                <div className='fazliddinheader' >
+                    <Link to="/"   >
+                        <img src={logo} alt="" />
+                    </Link>
+                    <div className="select" onClick={() => setOpen(!open)}>
+                        <div>
+                            <img
+                                src="https://upload.wikimedia.org/wikipedia/commons/f/f3/Flag_of_Russia.svg"
+                                alt="flag"
+                                className="flag"
+                            />
+                            <p><b>+998 (90)-606-66-66</b></p>
 
+                            <button className='fbutton' >оставить заявку</button>
+                        </div>
+
+
+
+                    </div>
+
+
+                </div>
             </div>
             <hr />
             <div>
-                <ul>
+                <ul className='fazliddinul' >
                     <li><Link to="/brand">о бренде</Link></li>
                     <li><Link to="afzalliklar">преимущества</Link></li>
                     <li><Link to="openclub">открыть клуб</Link></li>
