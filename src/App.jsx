127ef0c7874c5b75ed228b84229190700e7539a4
alfarruq <mengboyevfarrux01@gmail.com>
2026-01-21 15:17:35 +0500

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,7 @@ function App() {
   return (
     <div className='container' >
       <Header />
-
-
+      
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/brand' element={< Brand />} />
